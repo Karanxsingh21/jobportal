625eb2d3a1cad0abc6aba5c51c2d669bdb5556e7
unknown <Karanxsingh21@gmail.com>
2025-11-30 16:35:23 +0530

    deploy_04

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://caransingh04:ZkFVzlTyNrAxAEAT@cluster0.exkex3o.mongodb.net/");
+        await mongoose.connect("mongodb+srv://caransingh04:ZkFVzlTyNrAxAEAT@cluster0.exkex3o.mongodb.net/?retryWrites=true&w=majority");
         console.log('mongodb connected successfully');
     } catch (error) {
         console.log(error);
